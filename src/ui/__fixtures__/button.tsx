24f8c09dcc20e272ba38da03cb93ed8bfbf68b84
arihanv <arihanvaranasi@gmail.com>
2024-05-16 19:06:12 -0500

    update: create disabled state

--- src/ui/__fixtures__/button.tsx
+++ src/ui/__fixtures__/button.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Button } from '../button';
 
 const buttonFixtures = {
-  large: <Button size="lg">Button</Button>,
-  medium: <Button size="md">Button</Button>,
-  small: <Button size="sm">Button</Button>,
+  large: <Button size="lg" disabled={false}>Button</Button>,
+  medium: <Button size="md" disabled={false}>Button</Button>,
+  small: <Button size="sm" disabled={false}>Button</Button>,
 };
 
 export default buttonFixtures;
