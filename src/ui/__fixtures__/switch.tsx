aae5c5478a451aabdd06c6a3c03f45e402387c29
arihanv <arihanvaranasi@gmail.com>
2024-06-10 18:25:13 -0500

    feat: create switch

---
+++ src/ui/__fixtures__/switch.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Switch} from '../switch'
+
+const switchFixtures = {
+	default: (
+		<Switch />
+	)
+}
+
+export default switchFixtures
+
---
+++ src/ui/switch.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import * as React from "react";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
+
+import { clsx } from "clsx";
+
+const Switch = React.forwardRef<
+	React.ElementRef<typeof SwitchPrimitives.Root>,
+	React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props }, ref) => (
+	<SwitchPrimitives.Root
+		className={clsx(
+			"peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-[1px] border-black shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-white data-[state=unchecked]:bg-zinc-200",
+			className,
+		)}
+		{...props}
+		ref={ref}
+	>
+		<SwitchPrimitives.Thumb
+			className={clsx(
+				"pointer-events-none block size-4 rounded-full bg-zinc-500 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-3 data-[state=unchecked]:translate-x-0 data-[state=checked]:bg-black data-[state=checked]:w-[20px]  mx-[0.05rem]",
+			)}
+		/>
+	</SwitchPrimitives.Root>
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
+
+export { Switch };
