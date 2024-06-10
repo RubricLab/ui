439977f8b6a91f88afdb3da6cd6250605d920735
Sarim Malik <sarimrasheed@gmail.com>
2024-06-10 13:00:15 -0400

    Remove files

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import "./styles.css"
-
-export * from './ui/button'
-export * from './ui/tooltip'
--- src/ui/__fixtures__/heading.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-export default <div className='text-2xl font-bold'>Hello</div>
-
