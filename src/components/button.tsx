e8100d6346b991628c8a0f1af4fffbb49c13203c
Sarim Malik <sarimrasheed@gmail.com>
2024-06-13 15:21:39 -0400

    Fix button types

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,11 @@
-import { forwardRef, type ReactNode } from "react";
+import { forwardRef, type ButtonHTMLAttributes, type ReactNode } from "react";
 import { cn } from "../utils/cn";
 
 const variants = { primary: "", secondary: "" };
 
 const sizes = { small: "", medium: "", large: "" };
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   variant?: keyof typeof variants;
   size?: keyof typeof sizes;
@@ -16,7 +16,18 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant = "primary", size = "medium", ...props }, ref) => {
     return (
       <button
-        className={cn("", variants[variant], sizes[size], className)}
+        style={{
+          background: "white",
+          borderColor: "black",
+          borderWidth: "1px",
+          padding: "5px 10px",
+        }}
+        className={cn(
+          "underline underline-offset-4",
+          variants[variant],
+          sizes[size],
+          className
+        )}
         ref={ref}
         {...props}
       />
