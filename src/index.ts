c5b43c867cf6bd4e8eeff1c553f2a7e33516e125
Arihan Varanasi <63890951+arihanv@users.noreply.github.com>
2024-06-19 23:40:04 -0500

    nit: add semicolon

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import "./styles.css";
 export * from "./components/button";
 export * from "./components/input";
 export * from "./components/checkbox";
-export * from "./components/textarea"
+export * from "./components/textarea";
