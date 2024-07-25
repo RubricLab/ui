44d38575bc884dac5b231efb82cb5b76c9427c6d
arihanv <arihanvaranasi@gmail.com>
2024-07-24 22:34:27 -0500

    update: styling

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -2,7 +2,7 @@ import { type HTMLAttributes } from "react";
 import { cn } from "../utils/cn";
 
 const variants = {
-  default: "bg-rubricui-contrast text-rubricui-primary",
+  primary: "bg-rubricui-contrast text-rubricui-primary",
   secondary: "bg-rubricui-primary text-rubricui-contrast",
   destructive: "bg-red-500 text-white border-red-600",
   outline: "border-rubricui-primary text-rubricui-primary",
@@ -12,11 +12,11 @@ export interface BadgeProps extends HTMLAttributes<HTMLDivElement> {
   variant?: keyof typeof variants;
 }
 
-function Badge({ className, variant = "default", ...props }: BadgeProps) {
+function Badge({ className, variant = "primary", ...props }: BadgeProps) {
   return (
     <div
       className={cn(
-        "inline-flex border border-rubricui-contrast items-center rounded-md px-2.5 py-0.5 text-xs outline-none font-medium transition-all duration-rubricui-duration hover:ring-2 hover:ring-rubricui-contrast/10 hover:ring-offset-1",
+        "inline-flex border border-rubricui-primary/10 items-center rounded-md px-2.5 py-0.5 text-xs outline-none font-medium transition-all duration-rubricui-duration hover:ring-2 hover:ring-rubricui-contrast/10",
         variants[variant],
         className
       )}
