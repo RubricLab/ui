ede8e79912885ef6a91170ef7e2115f5508b3b92
Sarim Malik <sarimrasheed@gmail.com>
2025-08-13 13:25:51 -0400

    Add new layout and page components for chat and editor functionalities, including error handling and query execution. Implement new page creation logic with validation and user authentication checks. Update project and organization components for better navigation and user experience.

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -2,8 +2,9 @@ import type { LucideIcon } from 'lucide-react'
 import * as LucideIcons from 'lucide-react'
 import type { IconNames } from '../types'
 
-export const Icon = ({ name, ...props }: { name: IconNames } & React.ComponentProps<'svg'>) => {
+export const Icon = ({ name, size = 4 }: { name: IconNames; size?: number }) => {
 	const LucideIconComponent = LucideIcons[name] as LucideIcon | undefined
 	if (!LucideIconComponent) return null
-	return <LucideIconComponent {...props} />
+	// Multiply by 4 because in Tailwind 1 is equal to 4 px so size-1 is equal to width and height as 4 px
+	return <LucideIconComponent width={size * 4} height={size * 4} />
 }
